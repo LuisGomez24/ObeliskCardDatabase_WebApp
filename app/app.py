2882b188ddfe9d958ac48a69b04e32b14dae037d
LuisGomez24 <luisfergs24@gmail.com>
2023-01-17 03:52:46 -0600

    Added new route to seach cards and error 404 handler

--- app/app.py
+++ app/app.py
@@ -1,14 +1,34 @@
-from flask import Flask, render_template
+import json
+import requests
+from flask import Flask, render_template, request, redirect, url_for, flash, get_flashed_messages
 
-app = Flask(__name__)
+app = Flask(__name__.split('.')[0])
+app.secret_key = '87SjpqLnbaUJQ5zcV8ge0w'
+URL = 'https://db.ygoprodeck.com/api/v7/cardinfo.php'
 
-@app.route('/')
+@app.before_request
+def before_request():
+    pass
 
+@app.after_request
+def after_request(response):
+    return response
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    data = {
-        'title' : 'Ra’s Sanctuary'
-    }
-    return render_template('index.html', data=data)
+    if request.method == 'GET':
+        return render_template('index.html', fname='')
+    else:
+        return redirect(url_for('search', fname=request.form['fname']), code=302)
+
+@app.route('/search/<fname>', methods=['GET', 'POST'])
+def search(fname):
+    data = json.loads(requests.get(f'{URL}/?fname={fname}').text)
+    return render_template('index.html', data=data, fname=fname)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('page_not_found.html'), 404
 
 if __name__ == '__main__':
     app.run()
