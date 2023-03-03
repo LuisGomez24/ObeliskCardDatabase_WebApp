21d970e063df17528b53b143540f2408d92b68b9
LuisGomez24 <luisfergs24@gmail.com>
2023-03-03 02:54:36 -0600

    Added cache for API images and set images to response

--- app/app.py
+++ app/app.py
@@ -1,12 +1,18 @@
+import base64
 import json
 import requests
+import time
+from cachetools import TTLCache
 from flask import Flask, render_template, request, redirect, url_for, abort
-from flask_redis import FlaskRedis
+
+URL = 'https://db.ygoprodeck.com/api/v7/cardinfo.php'
+CACHE_SIZE = 2000
+CACHE_TTL = 10800
 
 app = Flask(__name__.split('.')[0])
-redis_client = FlaskRedis(app)
 app.secret_key = '87SjpqLnbaUJQ5zcV8ge0w'
-URL = 'https://db.ygoprodeck.com/api/v7/cardinfo.php'
+cache = TTLCache(maxsize=CACHE_SIZE, ttl=CACHE_TTL)
+cache_images = TTLCache(maxsize=CACHE_SIZE*3, ttl=CACHE_TTL)
 
 attribute_images = {
     "DARK"   : "https://i.ibb.co/pXMqkkF/Dark.webp"  ,
@@ -21,7 +27,7 @@ attribute_images = {
 }
 
 race_images = {
-    "Normal" : "hola",
+    "Normal" : "",
     "Continuous" : "",
     "Counter" : "",
     "Equip"   : "",
@@ -29,14 +35,6 @@ race_images = {
     "Quick-Play" : "",
     "Ritual" : ""
 }
-
-@app.before_request
-def before_request():
-    pass
-
-@app.after_request
-def after_request(response):
-    return response
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -57,26 +55,49 @@ def search(card):
                     return redirect(url_for('cardview', id=request.form['id']), code=302)
             except:
                 abort(400)
+    
+    data = json.loads(requests.get(f'{URL}/?fname={card}&num=15&offset=0').text)
+    data = set_cache(data)
         
-        
-    data = json.loads(requests.get(f'{URL}/?fname={card}').text)
     return render_template('index.html', data=data, card=card)
 
 @app.route('/card/<id>')
 def cardview(id):
-    card = json.loads(requests.get(f'{URL}/?id={id}').text)
-    #   card['data'][0]['race_image'] = race_images[card['data'][0]['race']]
-    if card['data'][0]['frameType'] == 'spell' or card['data'][0]['frameType'] == 'trap':
-        card['data'][0]['attribute_image'] = attribute_images[card['data'][0]['frameType'].upper()]
+    card = cache[id]
+    #   card['race_image'] = race_images[card['race']]
+    if card['frameType'] == 'spell' or card['frameType'] == 'trap':
+        card['attribute_image'] = attribute_images[card['frameType'].upper()]
     else:
-        card['data'][0]['attribute_image'] = attribute_images[card['data'][0]['attribute']]
+        card['attribute_image'] = attribute_images[card['attribute']]
     
-    print(card['data'][0]['desc'])
-    return render_template('cardview.html', card=card['data'][0])
+    return render_template('cardview.html', card=card)
 
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('page_not_found.html'), 404
 
+def set_cache(data):
+    for card in data['data']:
+        if card['id'] not in cache:
+            # If the card is not in the cache, store it
+            cache[str(card['id'])] = card
+        if card['card_images'][0]['image_url_small'] not in cache_images:
+            # If the card image is not in the cache, download and store it in the cache
+            card['card_image'] = get_image(card['card_images'][0]['image_url'])
+            time.sleep(0.05)
+    return data
+
+def get_image(url):
+    if url in cache_images:
+        # If the image is in the cache, return it directly
+        return cache_images[url]
+    else:
+        # If the image is not in the cache, download it from the API and store it in the cache
+        response = requests.get(url)
+        image = response.content
+        cache_images[url] = base64.b64encode(image).decode('utf-8')
+        return cache_images[url]
+
 if __name__ == '__main__':
     app.run()
+    
